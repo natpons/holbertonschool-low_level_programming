6ae679fe748695f82e9a28daf1b5ea7cd72d1eef
Natalia Ponsard <2503@holbertonschool.com>
2020-09-16 13:16:26 +0000

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<cstdio.h>
+
+/*
+*prints exactly "P.., followed by a new line
+*end with the value 0
+*/
+
+int main(void)
+{
+char string[ ] = "Programming is like building a multilingual puzzle";
+puts (string); 
+return (0); 
+}

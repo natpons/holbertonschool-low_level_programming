7755c4e9c5a3fd7401e2a6ee5e6f5a030d443280
Natalia Ponsard <2503@holbertonschool.com>
2020-09-18 15:18:52 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Display the alphabet letters without q and e
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if ((c != 'q') && (c != 'e'))
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}

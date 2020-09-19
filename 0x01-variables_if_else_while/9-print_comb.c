889ddf824d01c95321ca7e1fda48a1ce188874f5
Natalia Ponsard <2503@holbertonschool.com>
2020-09-19 18:11:24 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+	putchar(i + '0');
+	if (i != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	else
+	putchar(' ');
+	i++;
+}
+putchar('\n');
+
+return (0);
+}

7bf6eccd434cb0c1d7c40363cc54da8f04b13f5a
Natalia Ponsard <2503@holbertonschool.com>
2020-09-19 18:26:35 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-int i = 0;
+	int i = 0;
 
-while (i < 10)
-{
-	putchar(i + '0');
-	if (i != 9)
+	while (i < 10)
 	{
-	putchar(',');
-	putchar(' ');
+		putchar(i + '0');
+		if (i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		i++;
+		}
+	putchar('\n');
+	return (0);
 	}
-	i++;
-}
-return (0);
-}

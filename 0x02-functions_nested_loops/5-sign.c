c4818d146bcf78489e44f95c9ccca484508ec0f3
Natalia Ponsard <2503@holbertonschool.com>
2020-09-22 14:14:48 +0000

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,17 @@ int _print_sign(int n)
 	{
 	if (n > 0)
 	{
-	return (1);
 	_putchar('+');
-}
-
-	if (n == 0)
+	return (1);
+	}
+else if (n == 0)
 	{
-	return (0);
 	_putchar('0');
+	return (0);
 }
-:
-	if (n < 0)
-	{
-	return ('-1');
+else
+{
 	_putchar('-');
+	return (-1);
 }
 }

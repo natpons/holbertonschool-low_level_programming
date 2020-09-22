439f45521b2daecb2499d7dda5bfeb34bbf4295a
Natalia Ponsard <2503@holbertonschool.com>
2020-09-22 08:40:47 +0000

    task0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<string.h>
-#include"holberton.h"
+#include <stdio.h>
+#include <string.h>
+#include "holberton.h"
 
 /**
  * main - Write a program that prints Holberton, followed by a new line.
@@ -9,12 +9,14 @@
  */
 int main(void)
 {
-	char c[] = "Holberton\n";
-	unsigned int a = 0;
-	while (a < strlen(c))
+	char c[] = "Holberton";
+	int a = 0;
+
+	while (a < 9)
 	{
 		_putchar(c[a]);
 		a++;
 	}
+	_putchar('\n');
 	return (0);
 }

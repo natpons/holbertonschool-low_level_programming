f63b0181b39892254ce2229efb5a8acc2b9c5d36
Natalia Ponsard <2503@holbertonschool.com>
2020-09-22 16:22:20 +0000

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+*add - adds two integers and returns the result
+*@a: an int a
+*@b: an int b
+*
+*Return: the rezult of a+b
+*/
+
+int add(int a, int b)
+{
+int rez;
+
+rez = a + b;
+return (rez);
+}

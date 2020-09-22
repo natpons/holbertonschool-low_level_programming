530549db17eeec9350dff676f5f1c7141c59ff9c
Natalia Ponsard <2503@holbertonschool.com>
2020-09-22 12:15:22 +0000

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*int_islower(int c) - checks for lowercase character
+*@c: a char
+*
+*Return: 1 if c is lowercase, o if isn't
+*/
+
+int_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+}
+	else
+	{
+	return (0);
+	}
+}

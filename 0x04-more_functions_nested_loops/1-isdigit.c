89cfe4cd2eae61d8b3876e8657447f98d0c3f34c
Natalia Ponsard <2503@holbertonschool.com>
2020-09-24 17:32:07 +0000

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "holberton.h"
+
+/**
+ *_isdigit - Checks for a digit from 0 through 9.
+ *@c: a digit
+ *
+ *Return: 1 if c a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+	{
+		if (c >= 0 && c <= 9)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}

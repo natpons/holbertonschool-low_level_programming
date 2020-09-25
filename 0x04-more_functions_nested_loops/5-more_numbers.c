49225e91b214011080b7d467908e285ea04b857a
Natalia Ponsard <2503@holbertonschool.com>
2020-09-25 14:16:52 +0000

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,17 +3,15 @@
 
 /**
  * more_numbers - Prints 10 times the numbers, from 0 to 14
- *
- * Return: 0 (Success)
  */
+
 void more_numbers(void)
 {
-	int i;
-	int n = 0;
+	int i, n;
 
 	for (i = 0; i < 10; i++)
 	{
-	for (n = 0; n <=14; n++)
+	for (n = 0; n <= 14; n++)
 	{
 		if (n >= 10)
 			_putchar((n / 10) + '0');

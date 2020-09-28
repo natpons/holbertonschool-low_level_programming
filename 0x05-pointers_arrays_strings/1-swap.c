decfe3f45a79ae052a71ed18c08a5a10ea744684
Natalia Ponsard <2503@holbertonschool.com>
2020-09-28 17:01:14 +0000

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,14 @@
 
 /**
  * swap_int - swaps the values of two integers
+ * @a: a variable is a pointer to a int type
+ * @b: a variable is a pointer to a int type
+ *
  */
 void swap_int(int *a, int *b)
 {
 	int t;
+
 	t = *a;
 	*a = *b;
 	*b = t;

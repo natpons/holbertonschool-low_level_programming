ac13a36ba708a8027f15a442deddd4632b7cbb82
Natalia Ponsard <2503@holbertonschool.com>
2020-10-01 17:18:15 +0000

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @n: is the number of elements of the array
+ *
+ */
+
+void reserve_array(int *a, int n)
+{
+	for (i = n; i >= 0; i--)
+	{
+		_putchar (*a);
+	}
+
+}

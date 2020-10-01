fe7de31f8c3e02caef79896ae752ee11396b7ebd
Natalia Ponsard <2503@holbertonschool.com>
2020-10-01 15:02:10 +0000

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,20 +18,23 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcat - concatenates two strings dest и src
+ * *_strncat - concatenates two strings dest и src
  * @dest: a string to concate
  * @src: a string to add
+ * @n: number of bytes max from src
  *
  * Return: a string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = _strlen(src);
+	int len = _strlen(dest);
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[len + i] = src[i];
+
+	dest[len + i] = '\0';
 
 	return (dest);
 }

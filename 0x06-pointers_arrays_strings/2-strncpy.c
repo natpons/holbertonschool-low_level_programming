2311d0ca021f770d7e5ac89072013e9396c6d572
Natalia Ponsard <2503@holbertonschool.com>
2020-10-01 15:44:21 +0000

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strlen - Returnes the lenght of a string s
+ * @s: a pointer to a string
+ * Return: the lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * *_strcpy - copies a string
+ * @dest: a string destination
+ * @src: a string source
+ *
+ * Return: a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len2 =  _strlen(src);
+
+	for (i = 0; i <= len2 && i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+

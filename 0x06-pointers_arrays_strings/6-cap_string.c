de283eb75a3385c6cbad0105014dcc10f3ee96d8
Natalia Ponsard <2503@holbertonschool.com>
2020-10-02 15:54:59 +0000

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,17 @@
 
 char *cap_string(char *str)
 {
-	int i = 1;
-	int n = i - 1;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
+		int n = i - 1;
+
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
+			if (i == 0)
+				str[i] = str[i] - 32;
+
 			if (str[n] == ' ' || str[n] == '\t' || str[n] == '\n' ||
 			    str[n] == ',' || str[n] == ';' || str[n] == '.' ||
 			    str[n] == '!' || str[n] == '?' || str[n] == '"' ||

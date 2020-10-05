54e88285dd7be219928e2b04280e8d1768174a01
Natalia Ponsard <2503@holbertonschool.com>
2020-10-05 09:14:37 +0000

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * char *_memset - fills the first n bytes pointed to by s
+ * with the constant byte b
+ * @s: a pointer of n bytes
+ * @b: a constant byte
+ * @n: first n bytes of the memory area
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (s);
+}

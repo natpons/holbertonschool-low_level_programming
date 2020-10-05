dae2866d3c8ab1a5ab5b9d4dc37d657d77855240
Natalia Ponsard <2503@holbertonschool.com>
2020-10-05 14:21:44 +0000

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,26 @@
 #include "holberton.h"
 
 /**
- * unsigned int _strspn - gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  *
  * @s: initial segment
  * @accept: bytes for s
  *
- * Return:  the number of bytes in the initial segment of s 
+ * Return:  the number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, k = 0;
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0';j++);
+		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (s[i] == accept[j])
-					k++;
+			if (accept[j] == '\0')
+				return (i);
 		}
-		i++;
 	}
-	return(k);
-}	
+	return (i);
+}

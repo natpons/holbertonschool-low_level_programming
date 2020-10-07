81b88e015d354fbdc521b361d74f8dd0a9e2afe6
Natalia Ponsard <2503@holbertonschool.com>
2020-10-07 19:13:12 +0000

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns tha length of a string
+ * @s: a string
+ *
+ * Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+

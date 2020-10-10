13a92d8cc3bf3a1ce162cef5ef461fe9ff84ec04
Natalia Ponsard <2503@holbertonschool.com>
2020-10-10 13:37:28 +0000

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,19 +12,16 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 1, sum = 0;
+	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isalpha(*argv[i]) == 0)
-		{
-			sum += atoi(argv[i]);
-		}
-		else
+		if (isalpha(*argv[i]) != 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);

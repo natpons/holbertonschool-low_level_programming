18c444fe47ad36e53b4362534484641f70cd5615
Natalia Ponsard <2503@holbertonschool.com>
2020-10-11 10:59:47 +0000

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "holberton.h" 
 /**
  * main - adds positive numbers
  * @argc: argument count
- * @argv: argument values
+ * @*argv: argument values
  *
  * Return: print 0 - if no number + \n
- * print Error and return 1: if 1 of the number contains symbols are not digits
+ * print "Error and return 1": if 1 of the number contains symbols are not digits
  */
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		for (j = 0; argv[i][j] != '\0'; j++);
+		{
+		if ((argv[i][j] < '0') || (argv[i][j] > '9'))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
+		}
+		sum += *argv[i];
 	}
 
 	printf("%d\n", sum);

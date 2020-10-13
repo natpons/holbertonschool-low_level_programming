a2df464d4c108ce843aef3154f9c32e74559ceb9
Natalia Ponsard <2503@holbertonschool.com>
2020-10-13 11:58:32 +0000

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,18 +15,11 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(size * sizeof(char));
 
-	if (size > 0)
-	{
-		for (i = 0; i < size; i++)
-		{
-			arr[i] = c;
-		}
-		return (arr);
-	}
+	if ((size == 0) || (arr == 0))
+		return ('\0');
 
-	else if (size == 0)
-		return (0);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 
-	else
-	return (NULL);
+	return (arr);
 }

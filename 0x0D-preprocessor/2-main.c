2cddc72031fae2759c5a465d7ed144c663b90455
Natalia Ponsard <2503@holbertonschool.com>
2020-10-19 09:21:59 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from + \n.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* Prints location of C this C code */
+	printf("%s\n",__FILE__);
+	return (0);
+}

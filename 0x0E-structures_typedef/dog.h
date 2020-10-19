b04a600a7a308972d391b97cd8ec52139699c538
Natalia Ponsard <2503@holbertonschool.com>
2020-10-19 12:21:38 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYDOG
+#define MYDOG
+
+/**
+ * struct dog - definition a new type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+#endif

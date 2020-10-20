e8e8752a3f487b4a569eae2f685c8ccae401a747
Natalia Ponsard <2503@holbertonschool.com>
2020-10-20 11:25:20 +0000

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 /**
- * free_dog - creates a new dog
+ * free_dog - frees dogs
  *
  * @d: pointer to a dog's name
  *
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);

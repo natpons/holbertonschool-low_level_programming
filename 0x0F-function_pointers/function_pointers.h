89d4264128eb5f8215e87f9d3890d3dc4d94e1b4
Natalia Ponsard <2503@holbertonschool.com>
2020-10-21 10:13:54 +0000

    function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif

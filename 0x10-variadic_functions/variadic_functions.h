eda854bea7afc1004ac85b6da982d650f544a518
Natalia Ponsard <2503@holbertonschool.com>
2020-10-24 10:06:13 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

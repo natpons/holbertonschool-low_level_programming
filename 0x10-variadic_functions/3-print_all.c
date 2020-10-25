b60a30b6d6df665c86708edbaf1b0f2db68c7e9b
Natalia Ponsard <2503@holbertonschool.com>
2020-10-25 14:49:07 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -72,7 +72,7 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 
 	/*while input char string*/
-	while (format && format[i])
+	while ((format != NULL) && format[i])
 	{
 		j = 0;
 		/*while ops chars*/

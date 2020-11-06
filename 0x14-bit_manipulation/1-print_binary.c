54f15df4772ab4af9c713336391c6e75df1dccab
Natalia Ponsard <2503@holbertonschool.com>
2020-11-06 15:16:13 +0000

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,9 @@ void prbinary(unsigned long int n)
 {
 	if (n == 0)
 		return;
-	prbinary(n >> 1); 
+
+	prbinary(n >> 1);
+
 	if ((n & 1) == 1)
 		_putchar('1');
 	else
@@ -17,8 +19,8 @@ void prbinary(unsigned long int n)
 
 /**
  * print_binary - prints the binary representation of a number
- * @b: a pointer pointing to a string of 0 and 1 chars
- * 
+ * @n: the number to print
+ *
  * You are not allowed to use arrays, malloc, the % or / operators
  */
 void print_binary(unsigned long int n)

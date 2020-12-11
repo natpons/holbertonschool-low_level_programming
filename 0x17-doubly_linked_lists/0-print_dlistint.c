f54c4861941b5ceae82e91a4b20a98c8f183590f
Natalia Ponsard <2503@holbertonschool.com>
2020-12-11 08:26:12 +0000

    README, lists.h, task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: a pointer to the linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != 0)
+	{
+		printf("%d\n", h->n);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}

2b5955dd49108ce48d7ecf70be92a66be9ae19e5
Natalia Ponsard <2503@holbertonschool.com>
2020-12-11 12:05:09 +0000

    tasks 7 and 8

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @head: a pointer to the pointer to the doubly linked list
+ * @idx: index of the list where the new node should be added, starts at 0
+ * @n: a new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *temp, *hold, *newnode;
+
+	if (head == NULL)
+		return (0);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	newnode->prev = NULL;
+	if (idx == 0)/*Add at the beginning*/
+	{
+		newnode->next = NULL;
+		*head = newnode;
+		return (*head);
+	}
+	temp = *head;
+	while (temp != 0)/*Through the list till idx*/
+	{
+		if (i == idx)
+		{
+			hold->next = newnode;
+			newnode->prev = hold;
+			newnode->next = temp;
+			return (newnode);
+		}
+		hold = temp; /*pointer to a previous node*/
+		temp = temp->next; /*pointer to the next node*/
+		i++;
+	}
+	if (i == idx)/*at the end*/
+	{
+		temp->next = newnode;
+		newnode->next = NULL;
+		newnode->prev = temp;
+		return (newnode);
+	}
+	free(newnode);
+	return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index
+ * @head: a pointer to a pointer the  head
+ * @index: the index of the node that should be deleted
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ *
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *temp, *current;
+
+	temp = *head;
+	if (temp == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	for (i = 1; i < index; i++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (-1);
+	}
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+	return (1);
+}

8ba15eb41c9e9a239e05996013cbad118bb12d41
Natalia Ponsard <2503@holbertonschool.com>
2021-01-15 10:40:53 +0000

    fixed task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,6 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
+		return (1);
 
 	node->key = strdup(key);
 	node->value = strdup(value);
